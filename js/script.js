c87522bd7603d23ec0e606376813c08f400acd2d
Marc Cook <marcc24k@gmail.com>
2017-03-15 04:42:13 -0400

    Basic mockup is done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,1 @@
-alert("Hey guys! So, this is the basic set up for the website. Each part of the site has a little something so it's at least showing up in Github, the folders will not show up if they are empty. Feel free to adjust as you add content. Look forward to working with you guys!");
+//alert("Hey guys! So, this is the basic set up for the website. Each part of the site has a little something so it's at least showing up in Github, the folders will not show up if they are empty. Feel free to adjust as you add content. Look forward to working with you guys!");
