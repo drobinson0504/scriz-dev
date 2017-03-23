cf50436553d95befe08bed6723b4b41554eb5244
Marc Cook <marcc24k@gmail.com>
2017-03-23 13:01:04 -0400

    minor adjustments

--- js/script.js
+++ js/script.js
@@ -1,126 +1,217 @@
 //alert("Hey guys! So, this is the basic set up for the website. Each part of the site has a little something so it's at least showing up in Github, the folders will not show up if they are empty. Feel free to adjust as you add content. Look forward to working with you guys!");
 
 
- 
+
 function myMap() {
-var mapProp= {
-    center:new google.maps.LatLng(42.331429, -83.045753),
-    zoom:9,
-   
-     styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+    var mapProp = {
+        center: new google.maps.LatLng(42.331429, -83.045753)
+        , zoom: 9,
+
+        styles: [
             {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
+                elementType: 'geometry'
+                , stylers: [{
+                    color: '#242f3e'
+                }]
+            }
+            , {
+                elementType: 'labels.text.stroke'
+                , stylers: [{
+                    color: '#242f3e'
+                }]
+            }
+            , {
+                elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#746855'
+                }]
+            }
+            , {
+                featureType: 'administrative.locality'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#d59563'
+                }]
+            }
+            , {
+                featureType: 'poi'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#d59563'
+                }]
+            }
+            , {
+                featureType: 'poi.park'
+                , elementType: 'geometry'
+                , stylers: [{
+                    color: '#263c3f'
+                }]
+            }
+            , {
+                featureType: 'poi.park'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#6b9a76'
+                }]
+            }
+            , {
+                featureType: 'road'
+                , elementType: 'geometry'
+                , stylers: [{
+                    color: '#38414e'
+                }]
+            }
+            , {
+                featureType: 'road'
+                , elementType: 'geometry.stroke'
+                , stylers: [{
+                    color: '#212a37'
+                }]
+            }
+            , {
+                featureType: 'road'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#9ca5b3'
+                }]
+            }
+            , {
+                featureType: 'road.highway'
+                , elementType: 'geometry'
+                , stylers: [{
+                    color: '#746855'
+                }]
+            }
+            , {
+                featureType: 'road.highway'
+                , elementType: 'geometry.stroke'
+                , stylers: [{
+                    color: '#1f2835'
+                }]
+            }
+            , {
+                featureType: 'road.highway'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#f3d19c'
+                }]
+            }
+            , {
+                featureType: 'transit'
+                , elementType: 'geometry'
+                , stylers: [{
+                    color: '#2f3948'
+                }]
+            }
+            , {
+                featureType: 'transit.station'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#d59563'
+                }]
+            }
+            , {
+                featureType: 'water'
+                , elementType: 'geometry'
+                , stylers: [{
+                    color: '#17263c'
+                }]
+            }
+            , {
+                featureType: 'water'
+                , elementType: 'labels.text.fill'
+                , stylers: [{
+                    color: '#515c6d'
+                }]
+            }
+            , {
+                featureType: 'water'
+                , elementType: 'labels.text.stroke'
+                , stylers: [{
+                    color: '#17263c'
+                }]
             }
           ]
-};
+    };
+
+
+
+    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
     
- 
-    
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
-    
- marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: {lat: 42.331429, lng: -83.045753},
-    title: "Co-founder & Web Designers "
-     
-  });
-    
+    /*CAMERON*/
     marker = new google.maps.Marker({
-    map: map,
-    draggable: true,
-    animation: google.maps.Animation.DROP,
-    position: {lat: 42.201917, lng: -83.482812},
-    title: "Co-founder & Web Designer"
-  });
-     
+        map: map
+        , draggable: true
+        , animation: google.maps.Animation.DROP
+        , position: {
+            lat: 42.74469
+            , lng: -83.045773
+        }
+        , title: "Co-founder & Web Designer "
+
+    });
     
-  marker.addListener('click', toggleBounce);
+    /*DON*/
+    marker = new google.maps.Marker({
+        map: map
+        , draggable: true
+        , animation: google.maps.Animation.DROP
+        , position: {
+            lat: 42.331429
+            , lng: -83.045733
+        }
+        , title: "Co-founder & Web Designer "
+
+    });
     
+    /*NEAL*/
+    marker = new google.maps.Marker({
+        map: map
+        , draggable: true
+        , animation: google.maps.Animation.DROP
+        , position: {
+            lat: 42.331429
+            , lng: -83.045763
+        }
+        , title: "Web Designer "
+
+    });
     
+    /*MARC*/
+    marker = new google.maps.Marker({
+        map: map
+        , draggable: true
+        , animation: google.maps.Animation.DROP
+        , position: {
+            lat: 42.331429
+            , lng: -83.045753
+        }
+        , title: "Co-founder & Web Designer "
+
+    });
+
+    /*MARC*/
+    marker = new google.maps.Marker({
+        map: map
+        , draggable: true
+        , animation: google.maps.Animation.DROP
+        , position: {
+            lat: 42.201917
+            , lng: -83.482812
+        }
+        , title: "Co-founder & Web Designer"
+    });
+
+
+    marker.addListener('click', toggleBounce);
+
+
 }
 
 function toggleBounce() {
-  if (marker.getAnimation() !== null) {
-    marker.setAnimation(null);
-  } else {
-    marker.setAnimation(google.maps.Animation.BOUNCE);
-  }
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
 }
- 
